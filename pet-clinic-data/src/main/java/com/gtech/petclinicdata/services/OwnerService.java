1d1f77ca89b1d18aa609f2b14bf07e84414a0da0
jlgaskill1405 <jlgaskill1405@gmail.com>
2019-11-10 13:13:55 -0600

    Added Common Service interface. Updated service classes to extend the common interface. Closes #10.

--- pet-clinic-data/src/main/java/com/gtech/petclinicdata/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/gtech/petclinicdata/services/OwnerService.java
@@ -2,12 +2,8 @@ package com.gtech.petclinicdata.services;
 
 import com.gtech.petclinicdata.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
+
 }
